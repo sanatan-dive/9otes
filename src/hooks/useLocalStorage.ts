bea73fa5f30d6a1752d3f4247669125693454d23
sanatan-dive <sanatansharma352@gmail.com>
2024-10-21 23:35:37 +0530

    update

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import  { useEffect } from 'react'
 import { useState } from 'react'
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
